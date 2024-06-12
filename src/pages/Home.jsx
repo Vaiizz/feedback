bdd3b08cfafe4fb062b9ee11868abe5027b133dd
Felipe Vaz <felipeflores3108@gmail.com>
2024-06-12 13:49:17 +0000

    feat: adição da vierificação da nota e do envio da notapara mudar de pagina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,51 @@
-import React from 'react'
+import { useState } from 'react'
 import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from './Home.styles'
 
 import iconStarImg from "../assets/icon-star.svg"
 import thankYouImg from "../assets/illustration-thank-you.svg"
 
 export function Home() {
-  let aparecerResultado = true
+  const [mostrarResultado, setmostrarResultado] = useState(false)
+  const [feedbackNote, setFeedbackNote] = useState(0)
+
+  function handleFeedbackButtonClick(event) {
+    const feedback = Number(event.target.innerText)
+
+    setFeedbackNote(feedback)
+  }
+
+  function handleSubmit() {
+    if (feedbackNote === 0) return 
+
+    setmostrarResultado(true)
+  }
 
   return (
-    aparecerResultado === false ? (
+    mostrarResultado === false ? (
       <CardContainer>
         <IconContainer>
           <img src={iconStarImg} alt="icone de estrela" />
         </IconContainer>
-        
+
         <h1>Como foi o atendimento?</h1>
         <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradeçemos muito seu feedback para podermos melhorar nosso atendimento!</p>
 
         <ButtonContainer>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
+          <button onClick={handleFeedbackButtonClick}>1</button>
+          <button onClick={handleFeedbackButtonClick}>2</button>
+          <button onClick={handleFeedbackButtonClick}>3</button>
+          <button onClick={handleFeedbackButtonClick}>4</button>
+          <button onClick={handleFeedbackButtonClick}>5</button>
         </ButtonContainer>
 
-        <button>Enviar</button>
+        <button onClick={handleSubmit}>Enviar</button>
       </CardContainer>
     ) : (
       <CardContainer>
         <img src={thankYouImg} alt="imagem de agradecimento" />
 
         <ResultContainer>
-          <p>Você selecionou 4 de 5</p>
+          <p>Você selecionou {feedbackNote} de 5</p>
         </ResultContainer>
 
         <h1>Obrigado!</h1>
